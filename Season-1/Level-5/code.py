afa5ac0b7a3aa6eae19464fcbc3ced668098650b
jumainfomagnus <163571282+jumainfomagnus@users.noreply.github.com>
2024-04-03 14:26:15 +0000

    Level 5 completed

--- Season-1/Level-5/code.py
+++ Season-1/Level-5/code.py
@@ -1,9 +1,4 @@
-# Welcome to Secure Code Game Season-1/Level-5!
-
-# This is the last level of our first season, good luck!
-
 import binascii
-import random
 import secrets
 import hashlib
 import os
@@ -11,18 +6,17 @@ import bcrypt
 
 class Random_generator:
 
-    # generates a random token
+    # generates a random token using the secrets library for true randomness
     def generate_token(self, length=8, alphabet=(
     '0123456789'
     'abcdefghijklmnopqrstuvwxyz'
     'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     )):
-        return ''.join(random.choice(alphabet) for _ in range(length))
+        return ''.join(secrets.choice(alphabet) for i in range(length))
 
-    # generates salt
+    # generates salt using the bcrypt library which is a safe implementation
     def generate_salt(self, rounds=12):
-        salt = ''.join(str(random.randint(0, 9)) for _ in range(21)) + '.'
-        return f'$2b${rounds}${salt}'.encode()
+        return bcrypt.gensalt(rounds)
 
 class SHA256_hasher:
 
@@ -38,22 +32,34 @@ class SHA256_hasher:
         password_hash = password_hash.encode('ascii')
         return bcrypt.checkpw(password, password_hash)
 
-class MD5_hasher:
-
-    # same as above but using a different algorithm to hash which is MD5
-    def password_hash(self, password):
-        return hashlib.md5(password.encode()).hexdigest()
-
-    def password_verification(self, password, password_hash):
-        password = self.password_hash(password)
-        return secrets.compare_digest(password.encode(), password_hash.encode())
-
 # a collection of sensitive secrets necessary for the software to operate
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
 PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
-SECRET_KEY = 'TjWnZr4u7x!A%D*G-KaPdSgVkXp2s5v8'
-PASSWORD_HASHER = 'MD5_hasher'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+PASSWORD_HASHER = 'SHA256_hasher'
 
+# Solution explanation:
 
-# Contribute new levels to the game in 3 simple steps!
-# Read our Contribution Guideline at github.com/skills/secure-code-game/blob/main/CONTRIBUTING.md
+# Some mistakes are basic, like choosing a cryptographically-broken algorithm
+# or committing secret keys directly in your source code.
+
+# You are more likely to fall for something more advanced, like using functions that
+# seem random but produce a weak randomness.
+
+# The code suffers from:
+# - reinventing the wheel by generating salt manually instead of calling gensalt()
+# - not utilizing the full range of possible salt values
+# - using the random module instead of the secrets module
+
+# Notice that we used the “random” module, which is designed for modeling and simulation,
+# not for security or cryptography.
+
+# A good practice is to use modules specifically designed and, most importantly,
+# confirmed by the security community as secure for cryptography-related use cases.
+
+# To fix the code, we used the “secrets” module, which provides access to the most secure
+# source of randomness on my operating system. I also used functions for generating secure
+# tokens and hard-to-guess URLs.
+
+# Other python modules approved and recommended by the security community include argon2
+# and pbkdf2.
